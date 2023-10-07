1ce68f036aa2aa1493cdddc87c1cd31655872534
golivac2000 <golivac2@miumg.edu.gt>
2023-10-06 19:24:33 -0600

    Cambios

---
+++ ShoeShop-Ecommerce/client frontend/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Llamanos 24x7</h5>
+            <p>+502 5960 0921</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Ubiación</h5>
+            <p>Chiquimulilla</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>Whatsapp</h5>
+            <p>+502 2524 8921</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
---
+++ ShoeShop-Ecommerce/client frontend/src/components/profileComponents/ProfileTabs.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../LoadingError/Error";
+import Toast from "./../LoadingError/Toast";
+import Loading from "./../LoadingError/Loading";
+import { toast } from "react-toastify";
+import { updateUserProfile } from "../../Redux/Actions/userActions";
+
+const ProfileTabs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const toastId = React.useRef(null);
+
+  const Toastobjects = {
+    pauseOnFocusLoss: false,
+    draggable: false,
+    pauseOnHover: false,
+    autoClose: 2000,
+  };
+
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { loading: updateLoading } = userUpdateProfile;
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, [dispatch, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // Password match
+    if (password !== confirmPassword) {
+      if (!toast.isActive(toastId.current)) {
+        toastId.current = toast.error("Las contraseñas no coinciden", Toastobjects);
+      }
+    } else {
+      dispatch(updateUserProfile({ id: user._id, name, email, password }));
+      if (!toast.isActive(toastId.current)) {
+        toastId.current = toast.success("Perfil actualizado", Toastobjects);
+      }
+    }
+  };
+  return (
+    <>
+      <Toast />
+      {error && <Message variant="alert-danger">{error}</Message>}
+      {loading && <Loading />}
+      {updateLoading && <Loading />}
+      <form className="row  form-container" onSubmit={submitHandler}>
+        <div className="col-md-6">
+          <div className="form">
+            <label for="account-fn">Nombre de usuario</label>
+            <input
+              className="form-control"
+              type="text"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <div className="form">
+            <label for="account-email">Correo electrónico</label>
+            <input
+              className="form-control"
+              type="email"
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="form">
+            <label for="account-pass">Nueva contraseña</label>
+            <input
+              className="form-control"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="form">
+            <label for="account-confirm-pass">Confirmar contraseña</label>
+            <input
+              className="form-control"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <button type="submit">Actualizar perfil</button>
+      </form>
+    </>
+  );
+};
+
+export default ProfileTabs;
